3126791883d80666f94973551d1deb13aea7e758
Xisco Bibiloni <neo.xisco@gmail.com>
2021-02-13 00:46:17 +0100

    refactor: Add new loop to go over all the animal list

--- src/main/java/com/kata/Song.java
+++ src/main/java/com/kata/Song.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 class Song {
 
+    private static final int INITIAL_POSITION_LIST = 0;
 
     private static final String INITIAL_PHRASE = "There was an old lady who swallowed a ";
     private static final Map<String, String> ANIMAL_PHRASES = Map.of(
@@ -16,45 +17,43 @@ class Song {
     );
 
     public static void main(String[] args) {
+        StringBuilder builder = new StringBuilder();
 
-        String song = firstParagraph(args) +
-            "\n" +
-            middleParagraphFull(args, 2) +
-            "\n" +
-            middleParagraphFull(args, 3) +
-            "\n" +
-            middleParagraphFull(args, 4) +
-            "\n" +
-            middleParagraphFull(args, 5) +
-            "\n" +
-            middleParagraphFull(args, 6) +
-            "\n" +
-            lastParagraph(args);
+        builder.append(firstParagraph(args)).append("\n");
+        for (int i = 1; i < args.length-1; i ++) {
+            builder.append(middleParagraphFull(args, i)).append("\n");
+        }
+        builder.append(lastParagraph(args));
 
-        System.out.println(song);
+        System.out.println(builder.toString());
     }
 
     private static String firstParagraph(String[] args) {
 
-        return INITIAL_PHRASE + args[0] + ".\n" +
-            ANIMAL_PHRASES.get(args[0]) + "\n";
+        return INITIAL_PHRASE + args[INITIAL_POSITION_LIST] + ".\n" +
+            ANIMAL_PHRASES.get(args[INITIAL_POSITION_LIST]) + "\n";
     }
 
     private static String middleParagraphFull(String[] args, int animals) {
 
-        return INITIAL_PHRASE + args[animals-1] + ";\n" +
-            ANIMAL_PHRASES.get(args[animals-1]) + "\n" +
+        return INITIAL_PHRASE + args[animals] + ";\n" +
+            ANIMAL_PHRASES.get(args[animals]) + "\n" +
             shallowedPhrases(args, animals) +
-            ANIMAL_PHRASES.get(args[0]) + "\n";
+            ANIMAL_PHRASES.get(args[INITIAL_POSITION_LIST]) + "\n";
     }
 
     private static String shallowedPhrases(String[] args, int animals) {
-        String shallowedPhrases = "";
-        for (int i = animals-1; i > 0; i--) {
+        StringBuilder shallowedPhrasesBuilder = new StringBuilder("");
+        for (int i = animals; i > INITIAL_POSITION_LIST; i--) {
             String lastSymbol = i == 1 ? ";" : ",";
-            shallowedPhrases += "She swallowed the " + args[i] + " to catch the " + args[i-1] + lastSymbol + "\n";
+            shallowedPhrasesBuilder
+                    .append("She swallowed the ")
+                    .append(args[i]).append(" to catch the ")
+                    .append(args[i-1])
+                    .append(lastSymbol)
+                    .append("\n");
         }
-        return shallowedPhrases;
+        return shallowedPhrasesBuilder.toString();
     }
 
     private static String lastParagraph(String[] args) {
